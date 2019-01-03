182ef9ff3a2f3b4a8215eefd4fcf056f4131b1a5
jlhiskey <jasonhi@crazyredhead.com>
2019-01-02 16:53:02 -0800

    continued working thru the C# assignments

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Testing the text file creation.";
+
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Users\Jason Hiskey\Documents\CSharpClass.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
+ 
